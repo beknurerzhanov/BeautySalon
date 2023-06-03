c0153791e56b8bb6310c7382d2215b716ede6a88
beknurerzhanov <131442271+beknurerzhanov@users.noreply.github.com>
2023-06-03 12:18:32 +0600

    contacts.js

--- js/contacts.js
+++ js/contacts.js
@@ -1,45 +1,43 @@
-function playAudio(){
-    document.getElementById("audio").play();
-  }
-  
-  function playAudioG(){
-    document.getElementById("audiog").play();
-  }
-const form = document.getElementById('myForm');
+$('#myForm').submit(function(event) {
+    event.preventDefault();
 
-form.addEventListener('submit', function(event) {
-event.preventDefault();
-var fullName = document.getElementById('fullName').value;
-var email = document.getElementById('email').value;
-var fail = false;
-var adr_pattern = /[0-9a-z_]+@[a-z]+\.[a-z]{2,5}/i;
-if(fullName=="" || fullName ==" "){
+    var fullName = $('#fullName').val();
+    var email = $('#email').val();
+    var fail = false;
+    
+    var adr_pattern = /[0-9a-z_]+@[a-z]+\.[a-z]{2,5}/i;
+
+    if (fullName === "" || fullName === " ") {
       fail = "Your name please";
-  }
-  else if(email==""){
+    } else if (email === "") {
       fail = "Your email ";
-  }
-  else if(adr_pattern.test(email)==false){
-      fail = "Your email is not correct example:alibek.musabek@gmail.com";
-  }
-  if(fail){
-      playAudio();
+    } else if (!adr_pattern.test(email)) {
+      fail = "Your email is not correct example: alibek.musabek@gmail.com";
+    }
+
+    if (fail) {
       alert(fail);
       return;
-  }
-  else {
-      if (confirm('Thank you for your application.Do you want to play a game to get discount?')) {
-        playAudioG();
-  setTimeout(function() {
-    window.open('forms.html', '_blank');
-  }, 2000);
-}
-  }
-person = {
-  fullname: fullName,
-  Email: email
-};
-console.log("Your fullname is " + person.fullname);
-console.log("Your email is " + person.Email);
-form.reset();
-});
+    } else {
+      if (confirm('Thank you for your application. Do you want to play a game to get a discount?')) {
+        setTimeout(function() {
+          window.open('forms.html', '_blank');
+        }, 2000);
+      }
+    }
+
+    var person = {
+      fullname: "",
+      email: "",
+      Object: function (fullName, email) {
+        this.fullname = fullName;
+        this.email = email;
+      }
+    };
+    var nameemail = new person.Object($('#fullName').val(),$('#email').val())
+    console.log("Your fullname is " + nameemail.fullname);
+    console.log("Your email is " + nameemail.email);
+
+    $('#myForm')[0].reset();
+  });
+
